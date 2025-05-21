0fc6d82861906ce822ef3d40b38d84dae562eaa0
FabianAndrade14 <fabianandresm10@gmail.com>
2025-05-21 11:27:51 -0500

    fix(main): Added the main components

---
+++ src/app/components/navabar/navabar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navabar',
+  imports: [],
+  templateUrl: './navabar.component.html',
+  styleUrl: './navabar.component.scss'
+})
+export class NavabarComponent {
+
+}
---
+++ src/app/components/page-services/page-services.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-services',
+  imports: [],
+  templateUrl: './page-services.component.html',
+  styleUrl: './page-services.component.scss'
+})
+export class PageServicesComponent {
+
+}
